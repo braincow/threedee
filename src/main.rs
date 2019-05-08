9b1dc330a766550b39ffd7f4150297472b185526
Antti Peltonen <antti.peltonen@iki.fi>
2019-05-08 12:09:39 +0300

    divisinon by zero, work so far

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Point;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 
 use std::time::Duration;
 use std::vec::Vec;
@@ -265,12 +267,14 @@ pub fn main() {
             //println!("{:?}", normal);
             // phew! done.
 
-            fill_triangle(&points);
-
             // only draw the rectangle if it is visible
             if normal.x * (tri_normalize.p[0].x - dummy_camera.x) +
                 normal.y * (tri_normalize.p[0].y - dummy_camera.y) +
                 normal.z * (tri_normalize.p[0].z - dummy_camera.z) < 0.0 {
+
+                canvas.set_draw_color(Color::RGB(220, 220, 220));
+                fill_triangle(&points, &mut canvas);
+
                 // draw triangle (as lines) to backbuffer
                 for i in 0..3 {
                     // end of this line is the beginning of the next..
@@ -295,16 +299,98 @@ pub fn main() {
     }
 }
 
-fn fill_triangle(points: &[Point; 3]) {
+/*
+fillBottomFlatTriangle(Vertice v1, Vertice v2, Vertice v3)
+{
+  float invslope1 = (v2.x - v1.x) / (v2.y - v1.y);
+  float invslope2 = (v3.x - v1.x) / (v3.y - v1.y);
+
+  float curx1 = v1.x;
+  float curx2 = v1.x;
+
+  for (int scanlineY = v1.y; scanlineY <= v2.y; scanlineY++)
+  {
+    drawLine((int)curx1, scanlineY, (int)curx2, scanlineY);
+    curx1 += invslope1;
+    curx2 += invslope2;
+  }
+}
+*/
+fn fill_bottom_flat_triangle(points: &[Point; 3], canvas: &mut Canvas<Window>) {
+    //println!("fill_bottom_flat_triangle");
+    let invslope1: f64 = (points[1].x as f64 - points[0].x as f64) / (points[1].y as f64 - points[0].y as f64);
+    let invslope2: f64 = (points[2].x as f64 - points[0].x as f64) / (points[2].y as f64 - points[0].y as f64);
+    //println!("({}-{})/({}-{})={}", points[2].x, points[1].x, points[2].y, points[1].y, invslope2);
+
+    let mut curx1: f64 = points[0].x as f64;
+    let mut curx2: f64 = points[0].x as f64;
+    for scanline_y in points[0].y..points[1].y {
+        let p1 = Point::new(curx1 as i32, scanline_y);
+        let p2 = Point::new(curx2 as i32, scanline_y);
+        println!("bft {:?} -> {:?}", p1, p2);
+        canvas.draw_line(p1, p2).unwrap();
+        curx1 += invslope1;
+        curx2 += invslope2;
+    }
+}
+
+/*
+fillTopFlatTriangle(Vertice v1, Vertice v2, Vertice v3)
+{
+  float invslope1 = (v3.x - v1.x) / (v3.y - v1.y);
+  float invslope2 = (v3.x - v2.x) / (v3.y - v2.y);
+
+  float curx1 = v3.x;
+  float curx2 = v3.x;
+
+  for (int scanlineY = v3.y; scanlineY > v1.y; scanlineY--)
+  {
+    drawLine((int)curx1, scanlineY, (int)curx2, scanlineY);
+    curx1 -= invslope1;
+    curx2 -= invslope2;
+  }
+}
+*/
+fn fill_top_flat_triangle(points: &[Point; 3], canvas: &mut Canvas<Window>) {
+    //println!("fill_top_flat_triangle");
+    let invslope1: f64 = (points[2].x as f64 - points[0].x as f64) / (points[2].y as f64 - points[0].y as f64);
+    let invslope2: f64 = (points[2].x as f64 - points[1].x as f64) / (points[2].y as f64 - points[1].y as f64);
+    //println!("{:?}", points);
+    //println!("({}-{})/({}-{})={}", points[2].x, points[1].x, points[2].y, points[1].y, invslope2);
+
+    let mut curx1: f64 = points[2].x as f64;
+    let mut curx2: f64 = points[2].x as f64;
+    for scanline_y in (points[0].y..points[2].y).rev() {
+        let p1 = Point::new(curx1 as i32, scanline_y);
+        let p2 = Point::new(curx2 as i32, scanline_y);
+        println!("ftf {:?} -> {:?}", p1, p2);
+        canvas.draw_line(p1, p2).unwrap();
+        curx1 -= invslope1;
+        curx2 -= invslope2;
+    }
+}
+
+fn fill_triangle(points: &[Point; 3], canvas: &mut Canvas<Window>) {
     // http://www.sunshine2k.de/coding/java/TriangleRasterization/TriangleRasterization.html
     // sort the points for filling
-    println!("- {:?}", points);
+    //println!("- {:?}", points);
     let mut points_sorted = points.clone();
     // pdqsort sorts in decending order
     pdqsort::sort_by(&mut points_sorted, |a, b| b.y.cmp(&a.y));
     // .. we need them in ascending order
     points_sorted.reverse();
-    println!("+ {:?}", points_sorted);
+    //println!("+ {:?}", points_sorted);
+
+    if points_sorted[1].y == points_sorted[2].y {
+        // top-flat triangle
+        fill_top_flat_triangle(&points_sorted, canvas);
+    } else if points_sorted[0].y == points_sorted[1].y {
+        // bottom-flat triangle
+        fill_bottom_flat_triangle(&points_sorted, canvas);
+    } else {
+        // general case, we need to split the triangle in half
+        //let half_point: Point = 
+    }
 }
 
 // eof
